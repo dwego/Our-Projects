a07f14cd6fe824b80f11049f8beddf2c35c6996e
dwego <97767711+dwego@users.noreply.github.com>
2023-05-04 23:19:57 -0300

    speech_recognition.py

--- Python.py
+++ Python.py
@@ -1,26 +1,32 @@
 import webbrowser
 import speech_recognition as sr
 
-start = 'abrir google' or 'abrir chrome' or 'chrome' or 'google'
+# Defining the command to start the program
+start = 'open google' or 'open chrome' or 'chrome' or 'google'
 
-print('Estou ouvindo ')
-def ouvir_microfone():
-        microfone = sr.Recognizer()
-        with sr.Microphone() as source:
-            microfone.adjust_for_ambient_noise(source)
-            audio = microfone.listen(source)
-        try:
+print('Listening...')
 
-            frase = microfone.recognize_google(audio,language='pt-BR')
-            frase_leitura = frase.lower()
-            print(frase)
-            if frase_leitura == start:
-                webbrowser.open('www.google.com', new=0, autoraise=True)
-            else:
-                 webbrowser.open(f'{frase}', new=0, autoraise=True)
-            print('\033[36m''Voce falou: ''\033[36m' + f'\033[34m'+f'{frase}'+'\033[0;0m')
-            print('')
-        except sr.UnknownValueError:
-            print('\033[34m'+'Nao entendi'+'\033[0;0m')
+def listen_microphone():
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+    
+    try:
+        # Recognizing the spoken command
+        phrase = recognizer.recognize_google(audio, language='en-US')
+        phrase_leitura = phrase.lower()
+        
+        print(f'You said: {phrase}')
+        
+        # Opening Google if the command is recognized
+        if phrase_leitura == start:
+            webbrowser.open('www.google.com', new=0, autoraise=True)
+        else:
+            webbrowser.open(f'{phrase}', new=0, autoraise=True)
+        
+        print('')
+    except sr.UnknownValueError:
+        print('Sorry, I didn\'t understand that.')
 
-ouvir_microfone()
+listen_microphone()
